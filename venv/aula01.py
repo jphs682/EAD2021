760e1421635b1de228b0c5be9353034313f7ac92
lenovo <joaop9673@gmail.com>
2021-04-04 11:15:13 -0300

    teste

---
+++ venv/aula01.py
@@ -0,0 +1,7 @@
+n1 = int(input("informe um valor: "))
+n2 = int(input("informe o segundo valor: "))
+s = n1 + n2
+print("a soma dos valores {} e {} = {} ".format(n1, n2, s))
+
+
+print(max(123, 345))
